fd75130f5d2eecca0b510578f10ebb2157a47bba
Soul <joshgingold336@gmail.com>
2024-07-22 17:18:43 -0500

    signup modal functioning. Current bug: sends to wrong page

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,6 +1,5 @@
 // import Auth from "../utils/auth";
 // import { useMutation } from "@apollo/client";
-// // import { Form, Button, Alert } from "react-bootstrap";
 // import { useState } from "react";
 
 // export default LoginForm;
