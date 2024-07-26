ea69cbacf805e380995dbffe7513feb4395ebda6
tcarocci <t.23carocci@yahoo.com>
2024-07-25 21:38:22 -0500

    Implemented GraphQL mutations for frontend

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,31 @@
 import { gql } from "@apollo/client";
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
 export const ADD_PLANT = gql`
   mutation addPlant(
     $name: String!
