e53affcd90e36ae4c9f713cbebfab4b73365d0b1
unknown <ajgarza27@gmail.com>
2024-07-26 00:38:32 -0500

    Add new resolvers for GraphQL queries and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,48 +1,36 @@
-const { AuthenticationError } = require("apollo-server-express");
-const Plant = require("../plants");
-const Species = require("../species");
-
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 const resolvers = {
   Query: {
-    plants: async () => {
-      return await Plant.find().populate("species");
-    },
-    species: async () => {
-      return await Species.find();
-    },
-    plant: async (parent, { id }) => {
-      return await Plant.findById(id).populate("species");
-    },
-    speciesById: async (parent, { id }) => {
-      return await Species.findById(id);
+    me: async (parent, args, context) => {
+      if (context.user) {
+        console.log('User context:', context.user);
+        return User.findById(context.user._id);
+      }
+      console.log('No user in context');
+      throw new AuthenticationError('Not logged in');
     },
   },
   Mutation: {
-    addPlant: async (parent, args) => {
-      const plant = await Plant.create(args);
-      return plant.populate("species");
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      console.log('Generated token:', token); // Log the generated token
+      return { token, user };
     },
-    updatePlant: async (parent, { id, ...args }) => {
-      const plant = await Plant.findByIdAndUpdate(id, args, {
-        new: true,
-      }).populate("species");
-      return plant;
-    },
-    deletePlant: async (parent, { id }) => {
-      return await Plant.findByIdAndDelete(id);
-    },
-    addSpecies: async (parent, args) => {
-      const species = await Species.create(args);
-      return species;
-    },
-    updateSpecies: async (parent, { id, ...args }) => {
-      const species = await Species.findByIdAndUpdate(id, args, { new: true });
-      return species;
-    },
-    deleteSpecies: async (parent, { id }) => {
-      return await Species.findByIdAndDelete(id);
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
-
 module.exports = resolvers;
