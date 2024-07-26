e8533bf193c0bf2bb05a8ffbcdddf65a2662095a
Soul <joshgingold336@gmail.com>
2024-07-26 15:01:47 -0500

    Add user authentication and plant management. Updated resolvers to handle user auth and plant related operations. Defined Graphql queries, mutations, and type definitions. Also implemented user login and signup functionality with JWT auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,29 +1,33 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Plant, Species } = require("./models");
+const { signToken } = require("./utils/auth");
+
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        console.log('User context:', context.user);
         return User.findById(context.user._id);
       }
-      console.log('No user in context');
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError("Not logged in");
+    },
+    plants: async () => {
+      return Plant.find().populate("species");
+    },
+    plant: async (parent, { id }) => {
+      return Plant.findById(id).populate("species");
     },
   },
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
       const correctPw = await user.isCorrectPassword(password);
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
       const token = signToken(user);
-      console.log('Generated token:', token); // Log the generated token
       return { token, user };
     },
     addUser: async (parent, args) => {
@@ -31,6 +35,18 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    addPlant: async (parent, args) => {
+      return Plant.create(args);
+    },
+    updatePlant: async (parent, { id, ...args }) => {
+      return Plant.findByIdAndUpdate(id, args, { new: true }).populate(
+        "species"
+      );
+    },
+    deletePlant: async (parent, { id }) => {
+      return Plant.findByIdAndRemove(id);
+    },
   },
 };
+
 module.exports = resolvers;
