1131420174cb2b582f79d6e8f1e5fcf20f26023f
unknown <ajgarza27@gmail.com>
2024-07-28 22:50:35 -0500

    integrate Navbar, SearchBar, PlantList, and InfoBoxes into App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import InfoBoxes from "./components/infoBoxes";
-
+import React, { useState } from 'react';
+import 'bulma/css/bulma.min.css';
+import Navbar from './components/Navbar';
+import InfoBoxes from './components/infoBoxes';
+import SearchBar from './components/SearchBar';
+import PlantList from './components/PlantList';
 const App = () => {
+  const [plants, setPlants] = useState([]);
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <div className="container mt-5">
+        <h1 className="title has-text-centered">HOME PAGE</h1>
+        <SearchBar setPlants={setPlants} />
+        <PlantList plants={plants} />
+      </div>
       <InfoBoxes />
     </div>
   );
 };
-
 export default App;
